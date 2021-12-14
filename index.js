4fff38771491b3a55f7712786291d2cb4d1f4605
douglassmoss <douglassmoss@icloud.com>
2021-12-14 07:44:10 -0800

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const scuberHq = 42
+
+function distanceFromHqInBlocks(someValue) {
+  let distance = someValue - scuberHq;
+  return Math.abs(distance);
+} 
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks (location) * 264}
+
+  function distanceTravelledInFeet(start, destination) {
+    let travelledInFeet = (start - destination) * 264
+    return Math.abs(travelledInFeet);
+  }
+  calculatesFarePrice(start, destination)
+  const free = 400;
+  let result = null;
+  if (distanceTravelledInFeet <= free){
+    let result = 0;
+    return result
+  } else if (distanceTravelledInFeet > free < 2000){
+    let result =
+    distanceTravelledInFeet * .02;
+    return result
+  }else if (distanceTravelledInFeet > 2000 <= 2500) {
+    let result = 25;
+    return result
+  }else {
+    console.log('cannot travel that far')
+  } 
